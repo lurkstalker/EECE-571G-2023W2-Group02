4c42833062199a9d6ea141709435c69fae7573fc
lurkstalker <tszhang1@student.ubc.ca>
2024-03-24 11:49:59 -0700

    Add delete appiontment

--- test/RoomRentalTest.js
+++ test/RoomRentalTest.js
@@ -27,6 +27,31 @@ describe("RoomRental contract", function () {
 
     });
 
+    it("Rentee should be able to delete a room", async function () {
+        await roomRental.connect(rentee).userSignUp("alex", "12345");
+        await roomRental.connect(rentee).userLogin("12345");
+        await roomRental.connect(rentee).addRoom("Downtown", "Nice view", ethers.parseEther("1"));
+        expect(await roomRental.connect(rentee).getRoomLocation(1)).to.equal("Downtown");
+        expect(await roomRental.connect(rentee).getRoomIntro(1)).to.equal("Nice view");
+        const roomRentalPrice = await roomRental.connect(rentee).getRoomPrice(1);
+        assert.equal(roomRentalPrice, BigInt(etherAmountStr));
+        expect(await roomRental.connect(rentee).getTotalRoomCount()).to.equal(1);
+        expect(await roomRental.connect(rentee).isRoomAvailable(1)).to.equal(true);
+        await roomRental.connect(rentee).deleteRoom(1);
+        expect(await roomRental.connect(rentee).getTotalRoomCount()).to.equal(0);
+        expect(await roomRental.connect(rentee).isRoomAvailable(1)).to.equal(false);
+
+    });
+
+    it("Rentee should not be able to delete a room if operator is not the room owner", async function () {
+        await roomRental.connect(rentee).userSignUp("alex", "12345");
+        await roomRental.connect(rentee).userLogin("12345");
+        await roomRental.connect(rentee).addRoom("Downtown", "Nice view", ethers.parseEther("1"));
+        await roomRental.connect(renter1).userSignUp("tim", "6789");
+        await roomRental.connect(renter1).userLogin("6789");
+        await expect(roomRental.connect(renter1).deleteRoom(1)).to.be.revertedWith("Only the room owner can delete the room.");
+    });
+
     it("Renter can make an appointment if the room is available", async function () {
         await roomRental.connect(rentee).userSignUp("alex", "12345");
         await roomRental.connect(rentee).userLogin("12345");
@@ -100,4 +125,5 @@ describe("RoomRental contract", function () {
         // Try to make another appointment for the same room
         await expect(roomRental.connect(renter1).makeAppointment(1)).to.be.revertedWith("Appointment already exists for this room");
     });
+    
 });
