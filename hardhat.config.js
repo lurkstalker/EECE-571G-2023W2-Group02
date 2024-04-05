ecd7ff2a57684278469d72a99be3799a63d64517
lurkstalker <tszhang1@student.ubc.ca>
2024-04-05 13:31:30 -0700

    Add the sm depolyment code and deploy that to Sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.24",
-};
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
+}
