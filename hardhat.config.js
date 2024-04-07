824a864726ef8ad0094eba64100a4d8002972ed9
lurkstalker <tszhang1@student.ubc.ca>
2024-04-06 17:44:41 -0700

    Adjust the contract and test file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,6 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-require('dotenv').config();
-require("@nomiclabs/hardhat-ethers");
-
-const { API_URL, PRIVATE_KEY } = process.env;
+require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.24",
-  defaultNetwork: "sepolia",
-  networks: {
-    hardhat: {},
-    sepolia: {
-      url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`]
-    }
-  }
-}
+};
